3e0c13b8738aee30b250117f3da1657563a2b6ad
Lisbeth127 <claudia.duhoo@gmail.com>
2019-01-06 17:08:47 +0100

    ajout du fichier de script langstat.sh : cours Openclassroom linux

---
+++ langstat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Vérification de la presence du parametre du fichier dictionnaire 
+if [ -z $1 ] || [ ! -e $1 ]
+then
+        echo "erreur de syntaxe : ./langstat.sh [nomDuDico] optionnel -nbMotsDico"
+        exit 1
+else
+        echo "traitement fichier $1"
+        # On compte le nombre d'occurrences pour une lettre de l'alphabet et on l'enregistre dans statsDico.txt
+        for lettre in {A..Z}
+        do
+            compteur=`grep $lettre $1 | wc -l`
+            echo "$compteur - $lettre" >> statsDico.txt
+        done
+
+        # On trie numériquement les stats puis on affiche le résultat
+        sort -nr statsDico.txt
+fi
+
+
+if [ $# -ge 2 ] && [ $2 = '-nbMotsDico' ]
+then
+  # si option -nbMotsDico, on compte le nombre de mots du dictionnaire et on l'affiche
+  echo "le nombre de mot du dictionnaire est de : `cat $1 | wc -l`"
+  # puis on trie numériquement les stats et on affiche le résultat
+  sort -nr statsDico.txt
+else
+  # on trie numériquement les stats et on affiche le résultat
+  sort -nr statsDico.txt
+fi
+
+# Suppression du fichier statsDico.txt
+rm statsDico.txt
